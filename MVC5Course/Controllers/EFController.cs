d48a877a01cb8eab2053625577598406adb6fb1b
Denny Chin <denny.chin@gmail.com>
2017-05-07 17:20:33 +0800

    Commit Custom Validate.

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -5,9 +5,11 @@ using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
 using System.Net;
+using System.Data.Entity.Validation;
 
 namespace MVC5Course.Controllers
 {
+    //[Authorize]
     public class EFController : Controller
     {
         FabricsEntities db = new FabricsEntities();
@@ -16,13 +18,16 @@ namespace MVC5Course.Controllers
         public ActionResult Index()
         {
             
-            var all = db.Product.AsQueryable();
+            var all = db.Product.AsQueryable();  // 只是查詢物件，不會真的查回資料(延遲載入，故不會立刻執行)，除非之後用 ToList()
+            // var all = db.Product.AsEnumerable(); // 會真的查回資料(立即載入 n 筆資料，會立刻執行)
 
             //var data = all.Where(p => p.Active == true).ToList();
 
             var data = all.Where(p =>
                 p.Active == true
-                && p.ProductName.Contains("TT"))
+                && p.ProductName.Contains("TT")
+                && p.Is刪除 == false
+                )
                 .OrderByDescending(p => p.ProductId);
 
             //var data = all.Where(p => p.Active == true);
@@ -64,7 +69,6 @@ namespace MVC5Course.Controllers
         [HttpPost]
         public ActionResult Edit(int id,Product product)
         {
-
             if (ModelState.IsValid)
             {
                 var item = db.Product.Find(id);             
@@ -84,10 +88,37 @@ namespace MVC5Course.Controllers
      
         public ActionResult Delete(int id)
         {
-            var item = db.Product.Find(id);
-            db.Product.Remove(item);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            var product = db.Product.Find(id);
+
+            // 先刪除關聯表格中資料
+            // <法一>利用導覽屬性查出關聯資料後逐一刪除他
+            //foreach (var item in product.OrderLine.ToList())
+            //{
+            //    db.OrderLine.Remove(item);
+            //}
+
+            //// <法二> 一次刪除
+            //db.OrderLine.RemoveRange(product.OrderLine);
+
+            //db.Product.Remove(product);
+
+            try
+            {
+                // 改為用標記 Flag 方式
+                product.Is刪除 = true;
+
+                db.SaveChanges();  // 最後一部才做 Commit，以免中途發生錯誤在 DB 中產生贓資料
+
+                return RedirectToAction("Index");
+
+            }
+            catch (DbEntityValidationException ex) // 用 try ~ catch 包住錯誤，以便於發生錯誤時看詳細錯誤資訊
+            {
+
+                throw ex;
+            }
+
+            
         }
 
 
@@ -103,6 +134,27 @@ namespace MVC5Course.Controllers
         //        return View(data);
         //    }  
         //}
+
+        public ActionResult Details(int id)
+        {
+            // 效能調教使用(直接下 SQL)
+            var data = db.Database.SqlQuery<Product>("SELECT * FROM dbo.Product WHERE ProductId=@p0" ,id)
+                .FirstOrDefault();
+
+            return View(data);
+        }
+
+        public ActionResult RemoveAll()
+        {
+            // 效能差
+            //db.Product.RemoveRange(db.Product);
+            //db.SaveChanges();
+
+            // 效能好
+            db.Database.ExecuteSqlCommand("Delete from dbo.Product");
+
+            return View();
+        }
 
 
     }
--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC5Course.Models.ValidationAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -8,9 +9,44 @@ using System.Web;
 namespace MVC5Course.Models
 {
 	[MetadataType(typeof(ProductPartial))]
-	public partial class Product
+	public partial class Product : IValidatableObject
 	{
-	}
+        public int 訂單數量
+        {
+            get {
+                return this.OrderLine.Count;
+
+                //return this.OrderLine.Count; // 效能不好，因會取出所有資料後才能計算筆數
+                //return this.OrderLine.Where(p => p.Qty > 400).Count();  // 效能不好，因會取出所有資料後才能計算筆數
+                //return this.OrderLine.Where(p => p.Qty > 400).ToList().Count; // 效能不好，因會取出所有資料後才能計算筆數
+                return this.OrderLine.Count(p => p.Qty > 400);  // 效能最好，會自動產生 SELECT COUNT(*) FROM OrderLine; 後傳回來
+
+            }
+        }
+
+        /// <summary>
+        /// 模型驗證(進階驗證)
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            // 輸入驗證結束後才會做此模型驗證 (已經 Model Binding 完成且已經經過輸入驗證後)
+            if (this.Price > 100 && this.Stock > 5)
+            {
+                yield return new ValidationResult("價格與數量不合理"
+                    ,new string[] { "Price","Stock"});
+            }
+
+            if (this.OrderLine.Count() == 0 && this.Stock > 0)
+            {
+                yield return new ValidationResult("庫存和訂單數量不匹配" ,
+                    new string[] { "Stock"});
+            }
+
+            yield break;
+        }
+    }
 
     //public class Product1
     //{
@@ -20,10 +56,11 @@ namespace MVC5Course.Models
     public partial class ProductPartial
     {
         public int ProductId { get; set; }
-        [Required(ErrorMessage = "請輸入商品名稱吧")]
-        [MinLength(3), MaxLength(30)]
-        [RegularExpression(".+-.+", ErrorMessage = "商品名稱格式錯誤")]
+        [Required(ErrorMessage = "請輸入商品名稱")]
+        [MinLength(1), MaxLength(80)]
+        //[RegularExpression(".+-.+", ErrorMessage = "商品名稱格式錯誤")]
 		[DisplayName("商品名稱")]
+        [商品名稱必須包含Will字串(ErrorMessage = "商品名稱必須包含Will字串!")]
         public string ProductName { get; set; }
 
         [Required]
@@ -37,7 +74,7 @@ namespace MVC5Course.Models
         public Nullable<bool> Active { get; set; }
 
         [Required]
-        [Range(0, 100, ErrorMessage = "請輸入正確的庫存範圍")]
+        [Range(0, 99999999, ErrorMessage = "請輸入正確的庫存範圍")]
         [DisplayName("商品庫存")]
         public Nullable<decimal> Stock { get; set; }
 
