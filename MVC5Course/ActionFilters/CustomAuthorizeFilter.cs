5366708b58802fc1e69d6d3d567a5ab9595a1078
Denny Chin <denny.chin@gmail.com>
2017-05-25 21:16:58 +0800

    客製 AuthorizeFilter (實作AuthorizeAttribute)，完成驗證與擷取網域 NT 帳號功能，並套用至 Controller 或 Action

---
+++ MVC5Course/ActionFilters/CustomAuthorizeFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.ActionFilters
+{
+    public class CustomAuthorizeFilter : AuthorizeAttribute
+    {
+        public static string NTAccount = "";
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+
+            var user = filterContext.HttpContext.User;
+
+            // If Authenticated Failed , return HttpUnauthorizedResult and redirect to login page
+            if (user == null || !user.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+            else
+            {
+                // Get NT Account from user : user.Identity.Name is like as 'XYZ\DennyChin'
+                GetLoggedNTAccountName(user.Identity.Name);
+            }
+           
+        }
+
+        // Get NT Account from 'Domain\NTAccount' string (eg. 'XYZ\DennyChin')
+        private void GetLoggedNTAccountName(string allDMAcc)
+        {
+            string[] aryNTAccStr = allDMAcc.Split(new Char[] { '\\' });
+            string ntAccount = aryNTAccStr[1];
+            NTAccount = NTAccount.ToUpper(); // 帳號一律轉成大寫
+        }
+    }
+}
--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC5Course.ActionFilters;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
@@ -8,8 +9,9 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
+    [CustomAuthorizeFilter]
     public class HomeController : BaseController
-    {
+    {        
         public ActionResult Index()
         {
             return View();
